7774b4ad5dc20dd9bc8b5b744b7d99ae0e8224a8
hwcloud <452828+ekiaa@user.noreply.gitee.com>
2024-01-04 17:07:29 +0800

    add update 23.9.6

--- aosp/vendor/common/android/cph/frameworks/base/core/include/hw_com_android_internal_os_Zygote.h
+++ aosp/vendor/common/android/cph/frameworks/base/core/include/hw_com_android_internal_os_Zygote.h
@@ -2,5 +2,5 @@
  * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
  * Description: jni helper function
  */
-
+bool HwCanUnshare();
 void HwMountEmulatedStorage(int runtime_flags);
